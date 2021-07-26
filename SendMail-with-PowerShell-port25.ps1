8cfc1f80aa2d4e2b9730ea78f9c9725c45ead15e
lee <kim_navy@hotmail.com>
2021-07-26 21:44:43 +0700

    PowerShell Test SentMail with port 25   on Office365

---
+++ SendMail-with-PowerShell-port25.ps1
@@ -0,0 +1,25 @@
+﻿# Get the credential
+$credential = Get-Credential
+
+
+# Users: Tab-delimited list with columns named Name, Email, SamAccountName
+$SourcePath = "D:\SendMail.csv"
+ 
+# Import: Import the comma-delimited list of users (if tab-delimited, add '-Delimiter "`t"')
+$Users = Import-Csv -Path $SourcePath
+## Build parameters
+$mailParams = @{
+    SmtpServer                 = '<tenant>.mail.protection.outlook.com'
+    Port                       = '25'
+    UseSSL                     = $true   
+    From                       = '<email>@xxxx.com'
+    To                         = $Users.Email
+    Subject                    = "Direct Send $(Get-Date -Format g)"
+    Body                       = 'This is a test email using Direct Send'
+    DeliveryNotificationOption = 'OnFailure', 'OnSuccess'
+}
+
+## Send the email
+Send-MailMessage @mailParams
+
+## PLease subscription Youtube : iLikeiT.info ##

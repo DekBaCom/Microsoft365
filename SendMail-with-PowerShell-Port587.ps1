2e4dde974ae8760f4657b8079140809e5177a54d
lee <kim_navy@hotmail.com>
2021-07-26 21:46:48 +0700

    Script test send mail  on Office365   with port 587

---
+++ SendMail-with-PowerShell-Port587.ps1
@@ -0,0 +1,27 @@
+﻿# Get the credential
+$credential = Get-Credential
+
+
+# Users: Tab-delimited list with columns named Name, Email, SamAccountName
+$SourcePath = "D:\SendMail.csv"
+ 
+# Import: Import the comma-delimited list of users (if tab-delimited, add '-Delimiter "`t"')
+$Users = Import-Csv -Path $SourcePath
+
+## Define the Send-MailMessage parameters
+$mailParams = @{
+    SmtpServer                 = 'smtp.office365.com'
+    Port                       = '587' # or '25' if not using TLS
+    UseSSL                     = $true ## or not if using non-TLS
+    Credential                 = $credential
+    From                       = 'xxx@xxx.com'
+    To                         = $Users.Email
+    Subject                    = "SMTP Client Submission - $(Get-Date -Format g)"
+    Body                       = 'This is a test email using SMTP Client Submission'
+    DeliveryNotificationOption = 'OnFailure', 'OnSuccess'
+}
+
+## Send the message
+Send-MailMessage @mailParams
+
+## PLease subscription Youtube : iLikeiT.info ##

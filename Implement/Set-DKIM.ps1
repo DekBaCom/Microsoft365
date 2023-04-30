faeb88ce9f1316208db16784394f6ddc73670595
LeeKung <abdulloh.eg@gmail.com>
2023-04-30 15:46:25 +0700

    Create Set-DKIM.ps1

---
+++ Implement/Set-DKIM.ps1
@@ -0,0 +1,11 @@
+﻿Connect-ExchangeOnline
+
+#Set DKIM
+New-DkimSigningConfig -DomainName <Domain> -Enabled $false
+Get-DkimSigningConfig -Identity <Domain> | Format-List Selector1CNAME, Selector2CNAME
+
+Set-DkimSigningConfig -Identity <Domain> -Enabled $true  
+
+#Ref: https://learn.microsoft.com/en-us/microsoft-365/security/office-365-security/email-authentication-dkim-configure?view=o365-worldwide
+#Testing DKIM  URL :  https://admin.microsoft.com/AdminPortal/?searchSolutions=DKIM#/homepage
+

fa3698387d31c28b9aa41f6d59e659d985e7da7d
LeeKung <abdulloh.eg@gmail.com>
2023-05-15 10:22:21 +0700

    Update 15-5-23

---
+++ Implement/PowershellCreateShareMailbox/Create-ShareMailbox-AssignOwner-AssignPermissionSendAS.ps1
@@ -0,0 +1,53 @@
+﻿
+Connect-ExchangeOnline
+
+#Single
+#สร้างShareMailboxAccountเดียว
+New-Mailbox -Name "<Name Mailbox>" -DisplayName "<DisplaynameSharemailbox>"  -Alias <AliasnameSharemailbox> –Shared -PrimarySmtpAddress <Email UPN Sharemailbox >
+
+#Add Owner to Sharemailbox one Mailbox 
+Add-MailboxPermission "<NameSharemail>" -User <name member> -AccessRights FullAccess -InheritanceType all –AutoMapping $False 
+
+#Bulk
+#Create Multi Mailbox
+$sharemailbox = Import-CSv -Path ".\sharemailbox.csv"
+$sharemailbox | ForEach-Object{
+New-Mailbox -Name $_.NameMailbox -DisplayName $_.DisplayName –Shared -PrimarySmtpAddress $_.PrimarySmtpAddress
+Write-host -f Green "Create Sharemailbox $_.Name  Complete"
+}
+
+
+#Bulk Add Owner to Sharemailbox 
+$AddOWNsharemail = Import-CSv -Path ".\ownsharemailbox.csv"
+$AddOWNsharemail | ForEach-Object{
+Add-MailboxPermission $_.NameMailbox -User $_.member -AccessRights FullAccess -InheritanceType all –AutoMapping $False 
+Write-host -f Green "Add member $_.member to  $_.NameMailbox  Complete"
+}
+
+#Bulk Assign SEND AS permissions for Shared Mailbox
+$AddOWNsharemail = Import-CSv -Path ".\ownsharemailbox.csv"
+$AddOWNsharemail | ForEach-Object{
+Add-RecipientPermission $_.NameMailbox -Trustee $_.member -AccessRights SendAs -confirm:$False 
+Write-host -f Green "Assign SEND AS permissions $_.member to  $_.NameMailbox Complete"
+}
+
+
+
+#Find Sharemailbox and Add Owner to Sharemailbox 
+Get-Mailbox -Filter '(RecipientTypeDetails -eq "SharedMailbox")' | Add-MailboxPermission -User "Chanakarn Korklin" -AccessRights FullAccess -InheritanceType all -AutoMapping $False
+
+Add-MailboxPermission  -User "Abdulloh Etaeluengoh"  ShMailLeeNaja@ilikeit.info  -AccessRights  FullAccess -InheritanceType all -AutoMapping $False
+
+#Assign SEND AS permissions for Shared Mailbox
+Add-RecipientPermission "<NameSharemail>" -Trustee <Member> -AccessRights SendAs -confirm:$False 
+
+#Assign SEND AS permissions for ALL Shared Mailbox (Bulk)
+
+
+
+Get-Mailbox -Filter '(RecipientTypeDetails -eq "SharedMailbox")' | Add-RecipientPermission -Trustee leekung2020 -AccessRights SendAs -confirm:$False
+
+Get-Mailbox -Identity ShMailLeeNaja@ilikeit.info | fl
+
+Disconnect-ExchangeOnline
+
